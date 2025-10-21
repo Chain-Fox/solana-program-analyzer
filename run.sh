16ff22d5a0ffb87054d7078365cbe312018e4d31
Chain-Fox <chain-fox-developer@chain-fox.com>
2025-10-22 00:29:52 +0800

    chore: bump rust-toolchain to 2025-10-02

--- run.sh
+++ run.sh
@@ -19,7 +19,7 @@ pushd $1
 export RUSTC=${BASE}/target/debug/solana-program-analyzer
 # export RUST_BACKTRACE=full
 # export LOCKBUD_LOG=info
-export LD_LIBRARY_PATH="/home/chain-fox/.rustup/toolchains/nightly-2025-08-14-x86_64-unknown-linux-gnu/lib/":$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH="/home/chain-fox/.rustup/toolchains/nightly-2025-10-02-x86_64-unknown-linux-gnu/lib/":$LD_LIBRARY_PATH
 
 # cargo build
 RUSTC_FLAGS="-C overflow-checks=no"
